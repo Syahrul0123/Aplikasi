9eb1a8aed679135660959f1f3aa602bad9f8bd04
oalahbro <epradana15@gmail.com>
2024-05-07 21:14:11 +0700

    test export

--- koneksi.php
+++ koneksi.php
@@ -1,10 +1,8 @@
-<?php 
-$koneksi = mysqli_connect("localhost","root","admin123","aplikasi");
+<?php
+$koneksi = mysqli_connect("localhost", "root", "admin123", "aplikasi");
 
 // Check connection
-if (mysqli_connect_errno()){
+if (mysqli_connect_errno()) {
 	echo "Koneksi database gagal : " . mysqli_connect_error();
-}else
-{echo "success" ;}
-
-?>
+} else {
+}
