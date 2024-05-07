d6d6e1eb081c3d78a02b168a098d669bade61dda
Syahrul0123 <msyahrul725@gmail.com>
2024-05-07 18:39:00 +0700

    new repo

---
+++ transaksi.php
@@ -0,0 +1,120 @@
+<?php
+    include('koneksi.php'); //agar index terhubung dengan database, maka koneksi sebagai penghubung harus di include
+
+?>
+<!DOCTYPE html>
+<html>
+<head>
+    <title>TRANSAKSI</title>
+
+</head>
+<body>
+
+    <?php
+
+    include ('tampilan/header.php');
+    include ('tampilan/footer.php');
+    include ('tampilan/sidebar.php');
+?>
+
+    <!-- main content -->
+    <div class="main-content bg-primary">
+        <section class="section">
+          <div class="section-header">
+            <h1>TRANSAKSI</h1>
+            <div class="section-header-breadcrumb">
+                <div class="breadcrumb-item active"><a href="#">Dashboard</a></div>
+              <div class="breadcrumb-item text-primary">Transaksi</div>
+            </div>
+        </div>
+         <div class="row">
+              <div class="col-md-12">
+                <div class="card">
+                  <div class="card-header">
+            <h3>TRANSAKSI PEMBAYARAN</h3> 
+                    <div class="card-header-form">
+              <form action="proses_transaksi.php" method="post">
+            </div>
+          </div>
+            <div class="input-group mb-3">
+             <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">id Petugas</span>
+               </div>
+                <input type="text" name="id_petugas" class="form-control" placeholder="id petugas" aria-label="masukkan id petugas" aria-describedby="basic-addon1">
+                </div>
+
+                <div class="input-group mb-3">
+             <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">NISN</span>
+               </div>
+                <input type="text" name="nisn" class="form-control" placeholder="nisn" aria-label="masukkan nisn" aria-describedby="basic-addon1">
+                </div>
+
+               <div class="input-group mb-3">
+             <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">Tanggal Bayar</span>
+               </div>
+                <input type="date" name="tgl_bayar" class="form-control" placeholder="tgl_bayar" aria-label="tanggal" aria-describedby="basic-addon1">
+                </div> 
+
+              <div class="input-group mb-3">
+              <div class="input-group-prepend">
+                <label class="input-group-text" for="inputGroupSelect01">Bulan Bayar</label>
+              </div>
+              <select class="custom-select" name= "bulan_dibayar" id="inputGroupSelect01">
+                <option selected>--pilih bulan--</option>
+                <option value="januari">Januari</option>
+                <option value="februari">Februari</option>
+                <option value="maret">Maret</option>
+                <option value="april">April</option>
+                <option value="mei">Mei</option>
+                <option value="juni">Juni</option>
+                <option value="juli">Juli</option>
+                <option value="agustus">Agustus</option>
+                <option value="september">September</option>
+                <option value="oktober">Oktober</option>
+                <option value="november">November</option>
+                <option value="desember">Desember</option>
+              </select>
+            </div>  
+
+              <div class="input-group mb-3">
+                <div class="input-group-prepend">
+                  <label class="input-group-text" for="inputGroupSelect01">Tahun Bayar</label>
+                </div>
+                <select class="custom-select" name="tahun_dibayar" id="inputGroupSelect01">
+                  <option selected>--pilih tahun--</option>
+                  <option value="2019">2019</option>
+                  <option value="2020">2020</option>
+                  <option value="2021">2021</option>
+                </select>
+              </div>
+
+              <div class="input-group mb-3">
+             <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">ID SPP</span>
+               </div>
+                <input type="text" name="id_spp" class="form-control" placeholder="id spp" aria-label="masukkan id" aria-describedby="basic-addon1">
+                </div>
+
+                <div class="input-group mb-3">
+             <div class="input-group-prepend">
+            <span class="input-group-text" id="basic-addon1">jumlah</span>
+               </div>
+                <input type="text" name="jumlah_bayar" class="form-control" placeholder="jumlah bayar" aria-label="masukkan nominal" aria-describedby="basic-addon1">
+                </div>
+
+            <div class="d-flex justify-content-center">
+              <button type="submit" class="btn btn-success">Bayar</button>
+
+
+            </form>
+            </div>
+
+
+             
+
+
+
+
+            <br/>   
